f775035767536dbb523d3d93ddccb3a72961bd72
Niv Aiges <71957436+nivaiges@users.noreply.github.com>
2022-05-15 01:36:09 -0400

    mongoose added
    added capabilities to send information to mongoose server in the form of array, (using %betadd)

--- app.mjs
+++ app.mjs
@@ -1,19 +1,49 @@
 import Discord from "discord.js";
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+import mongoose from "mongoose";
+import testSchema from "./test-schema.js";
 import token from "./token.js";
-
 import axios from "axios";
 import fs from "fs";
-//const fetch = require("node-fetch");
-var searchText = "Natepwn";
-var summonerID = "UP8QZ4VqbDyRS3XWxMZjRrdGxdYtPm8WqTCbZBgvZM_wBdI";
-client.login(token);
+//import 'dotenv/config';
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
 const prefix = "%";
-client.on("ready", () => {
+//const fetch = require("node-fetch");
+
+var searchText = "";
+var summonerID = "";
+
+
+/*function writeToDataBase(playerData)
+{
+  new testSchema({
+    message: 'writing to database!',
+  }).save();
+  console.log("I wrote to the database");
+}
+writeToDataBase();
+*/
+
+
+//token is key found in token.js
+client.login(token);
+
+
+//once client gives a response, it then asks the database for a response
+//after both successfully finish, it console's that the bot is logged in
+client.on("ready", async () => {
+  await mongoose.connect(
+    'mongodb+srv://niva:jasper8018@mydatabase.qqmsz.mongodb.net/test',
+    {
+      keepAlive: true,
+    }
+    
+  )
+
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-//command intake.
+//command handler/intake.
 // %win
 // %lose
 // %anything_else --> will not execute anything outside of the statement
@@ -23,18 +53,22 @@ client.on("messageCreate", (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  
+
   if (command == "win") {
     message.channel.send("You bet that they would win!");
     console.log("win");
+    searchForPlayer();
   } else if (command == "lose") {
     message.channel.send("You bet that they would lose!");
     console.log("lose");
   } else if (command.includes("betadd")) {
     message.channel.send("You added: " + args + " to bet list!");
-    searchText = args;
+    //searchText = args;
     //betAddPlayer(args);
-    searchForPlayer();
+    
+    let encodedPlayerName = encodeURI(args); 
+    console.log(encodedPlayerName);
+    searchForPlayer(encodedPlayerName);
   } else if (command != "lose" && command != "win") {
     message.channel.send(
       command + " is not a valid command \n Please use %win or %lose"
@@ -49,8 +83,9 @@ client.on("messageCreate", (message) => {
 //uses [na1.api.riotgames.com] infront of the link to be used for searching a specific region
 //add ?api_key + [riotAPIKey] as a passcode of sorts for accessing riot api
 
-function searchForPlayer(event) {
+function searchForPlayer(playerName) {
   // Set up the correct API call
+  searchText = playerName;
   var APICallString =
     "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
     searchText +
@@ -63,7 +98,6 @@ function searchForPlayer(event) {
       // Success
       console.log(response.data);
       betAddPlayer(response.data);
-      //getSpectatorInfo();
     })
     .catch(function (error) {
       // Error
@@ -83,40 +117,43 @@ function getSpectatorInfo(event) {
     "https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + summonerID + "?api_key=" + riotApiKey;
 
   axios.get(SpectatorAPICallString).then(function (response1) {
-      // Success
-      console.log(response1.data);
-      message.channel.send(response1.data);
-    }).catch(function (error1) {
-      // Error
-      console.log("(57) Error Caught: " + error1);
-    });
+    // Success
+    console.log(response1.data);
+    message.channel.send(response1.data);
+  }).catch(function (error1) {
+    // Error
+    console.log("(57) Error Caught: " + error1);
+  });
 }
 
 
-function betAddPlayer(player)
-{
-player = JSON.stringify(player);
-console.log(player);
-fs.writeFileSync('c:\\Users\\nivai\\first_vs_code\\Discord_Bot\\summoners.txt', player, err =>
-{
-  if(err)
-{
-  console.log(error);
-  return;
-}
+function betAddPlayer(player) {
+  player = JSON.stringify(player);
+  var playerArray = player.split(",");
+  new testSchema({
+    message: playerArray,
+  }).save();
 
-});
-
-
-fs.readFile('c:\\Users\\nivai\\first_vs_code\\Discord_Bot\\summoners.txt', 'utf8', (err,data) => {
-  if(err)
-  {
-    console.log(err);
-    return;
-  }
-console.log(data);
-});
-
-
+  console.log("I wrote to the database");
   console.log("Reading Data" + player);
 }
+
+
+
+ /*fs.writeFileSync('summoners.txt', player, err => {
+    if (err) {
+      console.log(error);
+      return;
+    }
+
+  });*/
+  //c:\\Users\\nivai\\first_vs_code\\Discord_Bot\\
+
+ /* fs.readFile('summoners.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
+*/
